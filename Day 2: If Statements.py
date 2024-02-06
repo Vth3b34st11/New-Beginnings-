25573dfd993d062075aba580be4efcf0bf7e226a
Vth3b34st11 <159171736+Vth3b34st11@users.noreply.github.com>
2024-02-07 00:49:22 +0800

    Add files via upload
    Day 2 of learning how to use Python, and I am starting to learn how to use "if statements." This was demonstrated in a football goal-counting prompt I made, whereby based on the number of goals the user has scored, the code writes an emotive prompt back to the user.

---
+++ Day 2: If Statements.py
@@ -0,0 +1,16 @@
+# This is a football goal evaluation system made using if statements on python
+print("This is a goal tally evaluation prompt I made using Python :)")
+goals = input("Goals Scored?")
+goals = int(goals)
+if goals > 25:
+    print("Golden Boot OP")
+    print("Alan Sheerer esque")
+elif goals > 20:
+    print("Star Scorer")
+elif goals > 10:
+    print("Keep going")
+else:
+    print("START SCORING ASSWIPE")
+
+# "if" and "elif" provide the programme with separate blocks of code
+# MY FIRST BLOCK OF CODE!!!!!!
