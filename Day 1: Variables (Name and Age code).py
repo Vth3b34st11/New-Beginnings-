d14bca1ba500e161e05886ba169d2edb0de17e46
Vth3b34st11 <159171736+Vth3b34st11@users.noreply.github.com>
2024-02-07 00:45:32 +0800

    Add files via upload
    This is the first small block of code I wrote during my first day of learning how to use Python. This is an age generator where you provide your year of birth, and the code generates your age based on the year you were born in. Baby steps, but I would say pretty decent for the first day.

---
+++ Day 1: Variables (Name and Age code).py
@@ -0,0 +1,12 @@
+# My first mini coding project: The Age Calculator
+print("This is my first Python mini experience :)")
+name = input("What is your name? ")
+print("Hello " + name)
+birth_year = input("Enter your birth year: ")
+age = 2024 - int(birth_year)
+print("You are " + str(age), "years old")
+# Day 1 Ends Here
+# Notes for Day 1
+# print() = What comes on the console when we run it
+# input() Requests user input
+
